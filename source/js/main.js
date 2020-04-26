acb660d1b74e72ad6d57712ea98b2118bdf5d77f
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-04-26 17:24:46 +0300

    Добавлен скрипт открытия меню

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,18 @@ let beforeButton = document.querySelector('.slider-block__button-before');
 let afterButton = document.querySelector('.slider-block__button-after');
 let scale = document.querySelector('.slider-block__scale');
 let imageBox = document.querySelector('.slider-block__image-box');
+let mainNav = document.querySelector('.main-nav');
+let openMenuButton = document.querySelector('.page-header__open-button');
+
+mainNav.style.height = "0px";
+openMenuButton.addEventListener('click', function(evt) {
+  let mainNavItemAmount = document.querySelectorAll('.site-list__item').length;
+  if (mainNav.style.height === "0px") {
+    mainNav.style.height = (mainNavItemAmount * 65) + 1  + "px";
+  } else {
+    mainNav.style.height = "0px";
+  }
+})
 
 beforeButton.addEventListener('click', function (evt) {
   scale.classList.remove('js-is-right');
