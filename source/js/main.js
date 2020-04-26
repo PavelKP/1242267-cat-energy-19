bfe1702318f9f52831c5dd7a1e76f2a035ea1d39
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-04-26 20:09:19 +0300

    Добавлена кнопка разворота меню

--- source/js/main.js
+++ source/js/main.js
@@ -4,14 +4,22 @@ let scale = document.querySelector('.slider-block__scale');
 let imageBox = document.querySelector('.slider-block__image-box');
 let mainNav = document.querySelector('.main-nav');
 let openMenuButton = document.querySelector('.page-header__open-button');
+let pageHeader = document.querySelector('.page-header__logo-panel');
 
 mainNav.style.height = "0px";
+pageHeader.classList.remove("page-header__logo-panel--opened");
+pageHeader.classList.add("page-header__logo-panel--closed");
+
 openMenuButton.addEventListener('click', function(evt) {
   let mainNavItemAmount = document.querySelectorAll('.site-list__item').length;
   if (mainNav.style.height === "0px") {
     mainNav.style.height = (mainNavItemAmount * 65) + 1  + "px";
+    pageHeader.classList.remove("page-header__logo-panel--closed");
+    pageHeader.classList.add("page-header__logo-panel--opened");
   } else {
     mainNav.style.height = "0px";
+    pageHeader.classList.remove("page-header__logo-panel--opened");
+    pageHeader.classList.add("page-header__logo-panel--closed");
   }
 })
 
